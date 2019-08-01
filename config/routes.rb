fcf1b08ba14156bb83a39a40d1a14c56e820f9a0
surferbv <surferbv@gmail.com>
2019-07-31 21:15:34 -0700

    2019-07-31: Realized that the app is api based and you need to create a folder sctructure as app/api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      :fruits, only: [:index, :create, :destroy, :update]
+      resources :fruits, only: [:index, :create, :destroy, :update]
     end
   end
 end
